687d5ce75102eed46b38f177860beb72b035ff7a
le-king <toualeuarnaud@gmail.com>
2026-06-14 07:07:54 -0700

    solve22

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,20 @@
-const path = require('path');
+const path = require("path");
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   outputFileTracingRoot: path.join(__dirname),
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'https', hostname: 'worldcup-tickets.s3.amazonaws.com' },
-      { protocol: 'https', hostname: 'flagcdn.com' },
+      {
+        protocol: "https",
+        hostname: "worldcup-tickets.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
     ],
-  }
+  },
 };
 
 module.exports = nextConfig;
